f3a087354c31afd671fda8123dcb97cbba751a5c
Icaro Sousa <icaro.py@gmail.com>
2024-09-04 22:06:50 +0100

    fix: http file

--- src/SnackHub.Gateway/Program.cs
+++ src/SnackHub.Gateway/Program.cs
@@ -1,22 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
+const string reverseProxySection = "ReverseProxy";
+
 // Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder
-    .Services.AddSwaggerGen()
+    .Services
     .AddReverseProxy()
-    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+    .LoadFromConfig(builder.Configuration.GetSection(reverseProxySection));
 
 var app = builder.Build();
 app.MapReverseProxy();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
 app.UseHttpsRedirection();
 app.Run();
